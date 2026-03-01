8efe3c2808528f4c48710520d0dafb715c65a7a6
ujangyudi1721 <ujang.yudi3122@gmail.com>
2026-03-01 23:47:57 +0700

    pemisahan dashboard dan input.html

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
-console.log("APP JS TERBACA");
-let myChart = null;
-document.addEventListener("DOMContentLoaded", function () {
+//    =============================
+//    SUPABASE CONECTION (SAFE)
+//    =============================
+
+if (!window.supabaseClient) {
       const supabaseUrl = "https://tbgirpsbfjjbnrleltod.supabase.co";
       const supabaseKey = "sb_publishable_ywY-FXj_zicmv7b9DNzmkg_oRau1eFI";
+      window.supabaseClient = window.supabase.createClient(
+            supabaseUrl,
+            supabaseKey,
+      );
+}
 
-      const db = supabase.createClient(supabaseUrl, supabaseKey);
+const db = window.supabaseClient;
+//    =============================
+//    LOAD DASHBOARD
+//    =============================
 
-      document
-            .getElementById("btnSimpan")
-            .addEventListener("click", async function () {
-                  console.log("TOMBOL DI KLIK");
+async function loadData() {
+      const { data, error } = await db.from("transactions").select("*");
+
+      if (error) {
+            console.error(error);
+            return;
+      }
+
+      let totalIncome = 0;
+      let totalExpense = 0;
+
+      data.forEach((item) => {
+            if (item.tipe === "income") totalIncome += Number(item.jumlah);
+            if (item.tipe === "expense") totalExpense += Number(item.jumlah);
+      });
+
+      const incomeEl = document.getElementById("totalIncome");
+      const expenseEl = document.getElementById("totalExpense");
+
+      if (incomeEl) incomeEl.innerText = totalIncome;
+      if (expenseEl) expenseEl.innerText = totalExpense;
+}
+
+//    ==========================
+//    SIMPAN TRANSAKSI
+//    ==========================
+
+document.addEventListener("DOMContentLoaded", function () {
+      const btn = document.getElementById("btnSimpan");
+
+      if (btn) {
+            btn.addEventListener("click", async function () {
                   const tanggal = document.getElementById("tanggal").value;
                   const tipe = document.getElementById("tipe").value;
                   const kategori = document.getElementById("kategori").value;
@@ -17,137 +55,43 @@ document.addEventListener("DOMContentLoaded", function () {
                   const keterangan =
                         document.getElementById("keterangan").value;
 
-                  const { data, error } = await db.from("transactions").insert([
-                        {
-                              tanggal,
-                              tipe,
-                              kategori,
-                              jumlah,
-                              keterangan,
-                        },
-                  ]);
+                  if (
+                        !tanggal ||
+                        !tipe ||
+                        !kategori ||
+                        !jumlah ||
+                        !keterangan
+                  ) {
+                        alert("Lengakapi data dulu!");
+                        return;
+                  }
+
+                  const { error } = await db
+                        .from("transactions")
+                        .insert([
+                              { tanggal, tipe, kategori, jumlah, keterangan },
+                        ]);
 
                   if (error) {
-                        alert("Error: " + error.message);
-                        console.log(error);
-                  } else {
-                        alert("Data berhasil disimpan!");
-                        loadData();
-                        const tanggal = (document.getElementById(
-                              "tanggal",
-                        ).value = "");
-                        const tipe = (document.getElementById("tipe").value =
-                              "Income");
-                        const kategori = (document.getElementById(
-                              "kategori",
-                        ).value = "");
-                        const jumlah = (document.getElementById(
-                              "jumlah",
-                        ).value = "");
-                        const keterangan = (document.getElementById(
-                              "keterangan",
-                        ).value = "");
+                        alert("Gagal simpan!");
+                        console.error(error);
+                        return;
                   }
 
-                  console.log("insert sukses");
-                  await loadData();
-            });
-      console.log("DOM SIAP");
-      loadData();
+                  alert("Data berhasil disimpan !");
 
-      function formatRupiah(angka) {
-            return new Intl.NumberFormat("id-ID", {
-                  style: "currency",
-                  currency: "IDR",
-                  minimumFractionDigits: 0,
-            }).format(angka);
+                  document.getElementById("tanggal").value = "";
+                  document.getElementById("kategori").value = "";
+                  document.getElementById("jumlah").value = "";
+                  document.getElementById("keterangan").value = "";
+            });
       }
-      async function loadData() {
-            const { data, error } = await db
-                  .from("transactions")
-                  .select("*")
-                  .order("tanggal", { ascending: false });
 
-            if (error) {
-                  console.log(error);
-                  return;
-            }
-            let html = "<h3>Daftar Transaksi </h3><ul>";
-            //let saldo = 0;
-            let totalIncome = 0;
-            let totalExpense = 0;
-            data.forEach((item) => {
-                  console.log(item.tipe);
+      //    --------------------------------------------------------
+      //    Jalankan load dashboard hanya kalau elemen ada
+      //    --------------------------------------------------------
 
-                  const tipe = item.tipe.toLowerCase().trim();
-                  const jumlah = parseFloat(item.jumlah) || 0;
-
-                  if (item.tipe === "income") {
-                        totalIncome += jumlah;
-                  } else {
-                        totalExpense -= jumlah;
-                  }
-
-                  html += ` <li>
-                                    ${item.tanggal} | ${item.tipe} | ${item.kategori} | Rp ${item.jumlah}
-                                    <button onclick="hapusData(${item.id})">Hapus</button>
-                                    </li>`;
-                  console.log(item.jumlah, typeof item.jumlah);
-            });
-            console.log("Total income:", totalIncome);
-            console.log("Total Expense: ", totalExpense);
-
-            const saldo = totalIncome - totalExpense;
-
-            document.getElementById("totalIncome").innerText =
-                  formatRupiah(totalIncome);
-            document.getElementById("totalExpense").innerText =
-                  formatRupiah(totalExpense);
-            document.getElementById("totalSaldo").innerText =
-                  formatRupiah(saldo);
-
-            const ctx = document.getElementById("chartKeuangan");
-            // hancurkan chart lama
-            if (myChart !== null) {
-                  myChart.destroy();
-            }
-            // Buat chart baru
-            myChart = new Chart(ctx, {
-                  type: "bar",
-                  data: {
-                        labels: ["Income", "Expense"],
-                        datasets: [
-                              {
-                                    label: "Total",
-                                    data: [totalIncome, totalExpense],
-                                    backgroundColor: ["green", "red"],
-                              },
-                        ],
-                  },
-                  options: {
-                        responsive: true,
-                        maintainAspectRatio: false,
-                  },
-            });
-            html += "</ul>";
-            html += `<h3>Total Saldo: Rp ${saldo}</h3>`;
-
-            document.getElementById("hasil").innerHTML = html;
+      if (document.getElementById("totalIncome")) {
+            loadData();
       }
-      window.hapusData = async function (id) {
-            const konfirmasi = confirm("Yakin mau hapus data ini?");
-            if (!konfirmasi) return;
-
-            const { error } = await db
-                  .from("transactions")
-                  .delete()
-                  .eq("id", id);
-
-            if (error) {
-                  alert("gagal hapus:" + error.mesage);
-            } else {
-                  alert("Data berhasil dihapus!");
-                  loadData();
-            }
-      };
 });
