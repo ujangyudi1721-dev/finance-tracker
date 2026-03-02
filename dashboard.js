9e3b76a2f275bb165f9ed5ac0f758511000bf2c8
ujangyudi1721 <ujang.yudi3122@gmail.com>
2026-03-02 23:53:30 +0700

    penambahan tampilan summary

--- dashboard.js
+++ dashboard.js
@@ -9,105 +9,96 @@ document.addEventListener("DOMContentLoaded", async () => {
       const expenseEl = document.getElementById("totalExpense");
 
       console.log(incomeEl, expenseEl, listEl);
+      async function loadData() {
+            const { data, error } = await supabaseClient
+                  .from("transactions")
+                  .select("*");
 
-      const { data, error } = await supabaseClient
-            .from("transactions")
-            .select("*");
-
-      console.log("DATA:", data);
-
-      if (error) {
-            console.error(error);
-            return;
-      }
-
-      //--------- Urutkan terbaru di atas (berdasarkan id atau created_at)
-      data.sort((a, b) => b.id - a.id);
-
-      // ------- fungsi untuk menampilkan di span dashboard ----
-      let totalIncome = 0;
-      let totalExpense = 0;
-
-      //------ fungsi perhitungan -------
-      data.forEach((item) => {
-            console.log("TYPE DARI DB: ", item.tipe);
-            const tipe = item.tipe?.trim().toLowerCase();
-            const jumlah = Number(item.jumlah) || 0;
-
-            // --- hitung total ---
-            if (tipe === "income") {
-                  totalIncome += jumlah;
-            } else if (tipe === "expense") {
-                  totalExpense += jumlah;
+            console.log("Data dari supabase:", data);
+            let totalIncome = 0;
+            let totalExpense = 0;
+            if (error) {
+                  console.error(error);
+                  return;
             }
 
-            //--- Elemen list ---
-            const li = document.createElement("li");
-            li.classList.add(tipe === "income" ? "income" : "expense");
+            //--------- Urutkan terbaru di atas (berdasarkan id atau created_at)
+            data.sort((a, b) => b.id - a.id);
 
-            li.innerHTML = `${tipe.toUpperCase()} - Rp ${jumlah.toLocaleString("id-ID")}
+            // ------- fungsi untuk menampilkan di span dashboard ----
+            //let totalIncome = 0;
+            //let totalExpense = 0;
+
+            //------ fungsi perhitungan -------
+            data.forEach((item) => {
+                  console.log("TYPE DARI DB: ", item.tipe);
+                  const tipe = item.tipe?.trim().toLowerCase();
+                  const jumlah = Number(item.jumlah) || 0;
+
+                  // --- hitung total ---
+                  if (tipe === "income") {
+                        totalIncome += jumlah;
+                  } else if (tipe === "expense") {
+                        totalExpense += jumlah;
+                  }
+
+                  //--- Elemen list ---
+                  const li = document.createElement("li");
+                  li.classList.add(tipe === "income" ? "income" : "expense");
+
+                  li.innerHTML = `${tipe.toUpperCase()} - Rp ${jumlah.toLocaleString("id-ID")}
             <button data-id="${item.id}" class="editBtn">Edit</button>
             <button data-id="${item.id}" class="deleteBtn">Hapus</button>
             `;
 
-            listEl.appendChild(li);
-      });
-
-      // --- EvenListener untuk delete ---
-      document.querySelectorAll(".deleteBtn").forEach((button) => {
-            button.addEventListener("click", async (e) => {
-                  const id = e.target.dataset.id;
-
-                  const confirmDelete = confirm(
-                        "Yakin mau hapus transaksi ini? ",
-                  );
-                  if (!confirmDelete) return;
-
-                  const { error } = await supabaseClient
-                        .from("transactions")
-                        .delete()
-                        .eq("id", id);
-
-                  if (error) {
-                        console.error(error);
-                        alert("Gagal menghapus");
-                        return;
-                  }
-
-                  // --- Hapus elemen dari tampilan
-                  e.target.parentElement.remove();
-
-                  // --- Reload halaman supaya total update (secara simple dulu)
-                  location.reload();
+                  listEl.appendChild(li);
             });
-      });
 
-      // --- even listener untuk edit button ---
-      document.querySelectorAll(".editBtn").forEach((btn) => {
-            btn.addEventListener("click", () => {
-                  const id = btn.getAttribute("data-id");
-                  window.location.href = `input.html?id=${id}`;
-                  /*
-                  const { data, error } = await supabaseClient
-                        .from("transactions")
-                        .select("*")
-                        .eq("id", id)
-                        .single();
+            const saldo = totalIncome - totalExpense;
 
-                  console.log(document.getElementById("tipe"));
-                  if (error) return console.error(error);
+            document.getElementById("totalIncome").innerText =
+                  "Rp " + totalIncome.toLocaleString("id-ID");
+            document.getElementById("totalExpense").innerText =
+                  "Rp " + totalExpense.toLocaleString("id-ID");
+            document.getElementById("totalSaldo").innerText =
+                  "Rp " + saldo.toLocaleString("id-ID");
 
-                  document.getElementById("tanggal").value = data.tanggal;
-                  document.getElementById("tipe").value = data.tipe;
-                  document.getElementById("kategori").value = data.kategori;
-                  ocument.getElementById("jumlah").value = data.jumlah;
-                  ocument.getElementById("keterangan").value = data.keterangan;
-*/
-                 // editId = id;
-                  //document.getElementById("submitBtn").innerText = "Update";
+            // --- EvenListener untuk delete ---
+            document.querySelectorAll(".deleteBtn").forEach((button) => {
+                  button.addEventListener("click", async (e) => {
+                        const id = e.target.dataset.id;
+
+                        const confirmDelete = confirm(
+                              "Yakin mau hapus transaksi ini? ",
+                        );
+                        if (!confirmDelete) return;
+
+                        const { error } = await supabaseClient
+                              .from("transactions")
+                              .delete()
+                              .eq("id", id);
+
+                        if (error) {
+                              console.error(error);
+                              alert("Gagal menghapus");
+                              return;
+                        }
+
+                        // --- Hapus elemen dari tampilan
+                        e.target.parentElement.remove();
+
+                        // --- Reload halaman supaya total update (secara simple dulu)
+                        location.reload();
+                  });
             });
-      });
-      // --- Tampilkan total ---
-      incomeEl.textContent = totalIncome.toLocaleString("id-ID");
-      expenseEl.textContent = totalExpense.toLocaleString("id-ID");
+
+            // --- even listener untuk edit button ---
+            document.querySelectorAll(".editBtn").forEach((btn) => {
+                  btn.addEventListener("click", () => {
+                        const id = btn.getAttribute("data-id");
+                        window.location.href = `input.html?id=${id}`;
+                  });
+            });
+      }
+      loadData();
 });
